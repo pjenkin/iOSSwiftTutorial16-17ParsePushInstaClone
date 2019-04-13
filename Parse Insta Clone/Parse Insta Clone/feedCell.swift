3dab6659ecfe490324528c2e8ac417c4c6cc9d53
Peter Jenkin <peternjenkin@gmail.com>
2019-04-13 14:40:06 +0100

    16-151 Prototype cell (now with height set correctly!) for posts in table view

---
+++ Parse Insta Clone/Parse Insta Clone/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  Parse Insta Clone
+//
+//  Created by Peter Jenkin on 13/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Parse Insta Clone/Parse Insta Clone/feedVC.swift
+++ Parse Insta Clone/Parse Insta Clone/feedVC.swift
@@ -9,17 +9,44 @@
 import UIKit
 import Parse
 
-class feedVC: UIViewController {
+class feedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // set up table view
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // minimum 2 delegate functions for tableView
+    // ... numberofrowsin  .. cellforrow....
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10       // temporary check for now
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = UITableViewCell()
+        // populate Prototype cell(s) with bespoke cell object
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // cell.textLabel?.text = "Checking table setup"
+        return cell
+    }
+    
+    
+    // using additional delegate function, set height of prototype cell programmatically  https://stackoverflow.com/a/46438939
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+    
 
     @IBAction func logoutBtnClicked(_ sender: Any) {
         PFUser.logOutInBackground {(error) in
