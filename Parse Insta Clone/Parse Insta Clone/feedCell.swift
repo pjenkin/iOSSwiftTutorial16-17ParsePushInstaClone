b1939875b48bab0d95f44d3e3428a895311bdf50
Peter Jenkin <peternjenkin@gmail.com>
2019-04-13 20:04:34 +0100

    16-159 Basis of like and comment data structures implemented

--- Parse Insta Clone/Parse Insta Clone/feedCell.swift
+++ Parse Insta Clone/Parse Insta Clone/feedCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class feedCell: UITableViewCell {
 
@@ -30,7 +31,50 @@ class feedCell: UITableViewCell {
     }
 
     @IBAction func likeBtnClicked(_ sender: Any) {
+        let likeObject = PFObject(className: "Likes")
+        likeObject["from"] = PFUser.current()!.username!        // person using at mo is one liking
+        likeObject["to"] = postuuidLabel.text
+        
+        likeObject.saveInBackground {(success, error) in
+            if error != nil
+            {
+                let alert = UIAlertController(title: "Error", message:error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                let button = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                alert.addAction(button)
+                UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+                // self.present(alert, animated: true, completion: nil)
+                // NB 1st: the Alert Controller, then 2nd: the button (as an AlertAction), then 3rd: add button action to the controller, 4th: present the alert to the user
+                // NB in this case as using TableViewCell, cannot present Alert so use underlying ViewController instead
+                print(error?.localizedDescription)
+            }
+            else
+            {
+                print("Like has been saved to server")
+            }
+        }
     }
     @IBAction func commentBtnClicked(_ sender: Any) {
+        // NB TODO text of comment not actually included here - needs another control?
+        let commentObject = PFObject(className: "Comments")
+        commentObject["from"] = PFUser.current()!.username!        // person using at mo is one commenting
+        commentObject["to"] = postuuidLabel.text
+        
+        commentObject.saveInBackground {(success, error) in
+            if error != nil
+            {
+                let alert = UIAlertController(title: "Error", message:error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                let button = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                alert.addAction(button)
+                UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+                // self.present(alert, animated: true, completion: nil)
+                // NB 1st: the Alert Controller, then 2nd: the button (as an AlertAction), then 3rd: add button action to the controller, 4th: present the alert to the user
+                // NB in this case as using TableViewCell, cannot present Alert so use underlying ViewController instead
+                print(error?.localizedDescription)
+            }
+            else
+            {
+                print("Comment has been saved to server")
+            }
+        }
     }
 }
