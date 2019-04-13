92100d574b6a3493f7aa7b15b4d6e674882d61d3
Peter Jenkin <peternjenkin@gmail.com>
2019-04-13 17:10:40 +0100

    16-154 Image Picker implemented, showing on upload view

--- Parse Insta Clone/Parse Insta Clone/uploadVC.swift
+++ Parse Insta Clone/Parse Insta Clone/uploadVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class uploadVC: UIViewController {
+class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var postImage: UIImageView!  // NB not imageView - that's in feed
     @IBOutlet weak var commentText: UITextView!
@@ -16,6 +16,10 @@ class uploadVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        postImage.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(uploadVC.selectImage))
+        postImage.addGestureRecognizer(gestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +28,45 @@ class uploadVC: UIViewController {
     }
 
     @IBAction func uploadBtnClicked(_ sender: Any) {
+    }
+    
+    /// select an image to upload - handler for image Tap gesturerecognized
+    @objc func selectImage()
+    {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self        // thus self (ViewController) mus subclass UIImagePickerControllerDelegate, UINavigationControllerDelegate
+        pickerController.sourceType = .photoLibrary // could use camera alternatively
+        pickerController.allowsEditing = true       // NB editing will require check in code (cf didFinishPickingMediaWithInfo)
+        present(pickerController, animated: true, completion: nil)
+    }
+
+    // pre-built handler for picker, ready to fill with code
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // postImage.image = info
+        // NB will require 'Privacy - Photo Library Usage Description' in PList.info
+        
+        
+        var selectedImageFromPicker: UIImage?   // workaround to get original image https://stackoverflow.com/a/53219069
+        
+        // postImage.image = info[UIImagePickerControllerEditedImage] as? UIImage // didn't work as original image slipped through unused - no edited image to use
+        
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        {   // NB Any type from dictionary - try to cast to UIImage
+            selectedImageFromPicker = editedImage
+        }
+        else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            selectedImageFromPicker = originalImage
+        }
+        
+        // cautious approach here!
+        if let selectedImage = selectedImageFromPicker
+        {
+            postImage.image = selectedImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
 }
